4b6fcdd4037e6b7425265ca74ed0b5b2da7de4ee
Zack Kitzmiller <zackkitzmiller@gmail.com>
2020-11-14 08:09:15 -0600

    Initial Commit

---
+++ fr33c0in/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fr33c0in
+//
+//  Created by Zack Kitzmiller on 11/14/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fr33c0in/fr33c0inApp.swift
@@ -0,0 +1,17 @@
+//
+//  fr33c0inApp.swift
+//  fr33c0in
+//
+//  Created by Zack Kitzmiller on 11/14/20.
+//
+
+import SwiftUI
+
+@main
+struct fr33c0inApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
